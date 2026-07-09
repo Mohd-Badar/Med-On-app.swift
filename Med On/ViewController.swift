0c3ea6682b8ab8f859c6fd1ea0a26701839d716d
Mohd Badar <mohd.badar9917@gmail.com>
2026-07-09 11:45:00 +0530

    Initial Commit

---
+++ Med On/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Med On
+//
+//  Created by Mohd Badar on 09/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
